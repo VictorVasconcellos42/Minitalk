11540f5324a3b11817c0d3f733139fa2e821a7ea
vde-vasc <vde-vasc@student.42.rio>
2022-08-22 23:13:34 -0300

    server with pritf

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:16:50 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/08/22 21:12:30 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/08/22 22:47:04 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "printf/ft_printf.h"
 
 int	main(void)
 {
