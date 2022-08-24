5b1f660f11a9677eed1baeefb95043063206c3fb
vde-vasc <vde-vasc@student.42.rio>
2022-08-23 23:32:37 -0300

    Server created 100% mandatory

--- server.c
+++ server.c
@@ -6,23 +6,48 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:16:50 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/08/22 22:47:04 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/08/23 22:00:30 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "printf/ft_printf.h"
+#include "minitalk.h"
+
+void	check_signal(int sig)
+
+{
+	static int	bin;
+	static char chr;
+
+	if (bin < 8)
+	{
+		if (sig == SIGUSR1)
+			chr += (1 << (7 - bin));
+		bin++;
+	}
+	if (bin == 8)
+	{
+		ft_printf("%c", chr);
+		chr = 0;
+		bin = 0;
+	}
+}
 
 int	main(void)
+
 {
-	pid_t	pid;
+	//struct sigaction sa;
+	int pid;
 
 	pid = getpid();
 	
-	ft_printf("%i", pid);
+	ft_printf("Server PID: %i\n", pid);
+	//sa.sa_handler = &check_signal;
+	signal(SIGUSR1, check_signal);
+	signal(SIGUSR2, check_signal);
 	while (1)
+	{
 		pause();
+		//sigaction(SIGUSR1, &sa, NULL);
+		//sigaction(SIGUSR2, &sa, NULL);
+	}
 }
