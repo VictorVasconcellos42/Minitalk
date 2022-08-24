1df147b9f6f254946c69a4ada81b3a61ea4eafbe
vde-vasc <vde-vasc@student.42.rio>
2022-08-24 14:22:20 -0300

    change path libft.h

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 17:52:38 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/08/23 23:18:39 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/08/24 14:14:48 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <unistd.h>
 # include <signal.h>
 # include <stdlib.h>
-# include "libft/ft_printf.h"
+# include "libft/libft.h"
 
 #endif
