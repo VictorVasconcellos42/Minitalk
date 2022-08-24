4cf257660daa99d3fb957b553bb9f53cffb073d2
vde-vasc <vde-vasc@student.42.rio>
2022-08-23 23:33:27 -0300

    header created

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 17:52:38 by vde-vasc          #+#    #+#             */
+/*   Updated: 2022/08/23 23:18:39 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include "libft/ft_printf.h"
+
+#endif
